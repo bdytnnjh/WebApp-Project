f6750fb55eb3c7e7f669ae2284c6418467e96537
HangSemey <huzulkipli@gmail.com>
2025-01-15 21:20:18 +0800

    Update 2025_01_14_083100_create_landmarks_table.php

--- database/migrations/2025_01_14_083100_create_landmarks_table.php
+++ database/migrations/2025_01_14_083100_create_landmarks_table.php
@@ -1,30 +1,12 @@
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up()
-    {
-        Schema::create('landmarks', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->text('description');
-            $table->float('latitude', 10, 6);
-            $table->float('longitude', 10, 6);
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('feedback');
-    }
-};
+public function up()
+{
+    Schema::create('landmarks', function (Blueprint $table) {
+        $table->id();
+        $table->string('landmark_name');
+        $table->string('short_term_name');
+        $table->string('name');
+        $table->decimal('latitude', 10, 7);
+        $table->decimal('longitude', 10, 7);
+        $table->timestamps();
+    });
+}
